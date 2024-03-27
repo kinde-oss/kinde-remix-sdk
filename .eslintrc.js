ca6acf1fee5f64c01f1d60f1359ee0962a662d73
Daniel Rivers <me@danielrivers.com>
2024-03-27 21:20:03 +0000

    chore: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "ignorePatterns": ["tests/*"],
+    "extends": "standard-with-typescript",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,60 +1,58 @@
-export type KindeUser = {
-  family_name: string | null;
-  given_name: string | null;
-  picture: string | null;
-  email: string;
-  id: string;
-};
+export interface KindeUser {
+  family_name: string | null
+  given_name: string | null
+  picture: string | null
+  email: string
+  id: string
+}
 
-export type KindeAccessToken = {
-  aud: string[];
-  azp: string;
+export interface KindeAccessToken {
+  aud: string[]
+  azp: string
   billing: {
-    has_payment_details: boolean;
-    org_entitlements: any[] | null;
+    has_payment_details: boolean
+    org_entitlements: any[] | null
     plan: {
-      code: string | null;
-      created_on: number | null;
-      has_trial_period: boolean | null;
-      invoice_due_on: number | null;
-      name: string | null;
-      plan_charge_type: number | null;
-      trial_expires_on: number | null;
-    };
-  };
-  exp: number;
-  feature_flgs: {
-    [key: string]: {
-      t: "i" | "b" | "s";
-      v: string | boolean | number;
-    };
-  };
-  iat: number;
-  iss: string;
-  jti: string;
-  org_code: string;
-  permissions: string[];
-  scp: string[];
-  sub: string;
-};
+      code: string | null
+      created_on: number | null
+      has_trial_period: boolean | null
+      invoice_due_on: number | null
+      name: string | null
+      plan_charge_type: number | null
+      trial_expires_on: number | null
+    }
+  }
+  exp: number
+  feature_flgs: Record<string, {
+    t: 'i' | 'b' | 's'
+    v: string | boolean | number
+  }>
+  iat: number
+  iss: string
+  jti: string
+  org_code: string
+  permissions: string[]
+  scp: string[]
+  sub: string
+}
 
-export type KindeIdToken = {
-  at_hash: string;
-  aud: string[];
-  auth_time: number;
-  azp: string;
-  email: string;
-  exp: number;
-  family_name: string;
-  given_name: string;
-  iat: number;
-  iss: string;
-  jti: string;
-  name: string;
-  org_codes: string[];
-  picture: string;
-  sub: string;
-  updated_at: number;
-};
+export interface KindeIdToken {
+  at_hash: string
+  aud: string[]
+  auth_time: number
+  azp: string
+  email: string
+  exp: number
+  family_name: string
+  given_name: string
+  iat: number
+  iss: string
+  jti: string
+  name: string
+  org_codes: string[]
+  picture: string
+  sub: string
+  updated_at: number
+}
 
-export type KindePermission = { isGranted: boolean; orgCode: string | null };
+export interface KindePermission { isGranted: boolean, orgCode: string | null }
