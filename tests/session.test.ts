2c1352d84db982856fa1e8501460d40205aef7b5
Peter Phanouvong <peterphanouvong@gmail.com>
2024-04-08 11:11:42 +1000

    tests: update sessionManager tests

--- tests/session.test.ts
+++ tests/session.test.ts
@@ -1,56 +1,63 @@
-import { expect, test, vi, Mock, describe } from "vitest";
+import { installGlobals } from "@remix-run/node";
+import { describe, expect, test } from "vitest";
 import { createSessionManager } from "../src/session";
-import { installGlobals } from "@remix-run/node"; // You might need to install this dependency
 
 installGlobals();
 
 describe("Session tests", () => {
   test("getSessionItem", async () => {
     const mockRequest = new Request("http://kinde.com");
-    const { session, sessionManager } = await createSessionManager(mockRequest);
+    const { sessionManager } = await createSessionManager(mockRequest);
 
-    session.set("someKey", "someValue");
+    sessionManager.setSessionItem("someKey", "someValue");
 
     expect(await sessionManager.getSessionItem("someKey")).toEqual("someValue");
   });
 
   test("setSessionItem", async () => {
     const mockRequest = new Request("http://kinde.com");
-    const { session, sessionManager } = await createSessionManager(mockRequest);
+    const { sessionManager } = await createSessionManager(mockRequest);
 
     await sessionManager.setSessionItem("someKey", "someNewValue");
 
-    expect(session.get("someKey")).toEqual("someNewValue");
+    expect(await sessionManager.getSessionItem("someKey")).toEqual(
+      "someNewValue"
+    );
   });
 
   test("removeSessionItem", async () => {
     const mockRequest = new Request("http://kinde.com");
-    const { session, sessionManager } = await createSessionManager(mockRequest);
+    const { sessionManager } = await createSessionManager(mockRequest);
 
-    session.set("someKey", "someValue");
+    await sessionManager.setSessionItem("someKey", "someValue");
 
-    expect(session.get("someKey")).toEqual("someValue");
+    expect(await sessionManager.getSessionItem("someKey")).toEqual("someValue");
     await sessionManager.removeSessionItem("someKey");
-    expect(session.get("someKey")).not.toBeDefined();
+    expect(await sessionManager.getSessionItem("someKey")).not.toBeDefined();
   });
 
   test("destroySession", async () => {
     const mockRequest = new Request("http://kinde.com");
-    const { session, sessionManager, sessionStorage } =
-      await createSessionManager(mockRequest);
+    const { sessionManager } = await createSessionManager(mockRequest);
 
     await sessionManager.setSessionItem("someKey", "someValue");
     await sessionManager.setSessionItem("someKey2", "someValue2");
     await sessionManager.setSessionItem("access_token", "asds");
 
-    expect(session.get("someKey")).toEqual("someValue");
-    expect(session.get("someKey2")).toEqual("someValue2");
-    expect(session.get("access_token")).toEqual("asds");
+    expect(await sessionManager.getSessionItem("someKey")).toEqual("someValue");
+    expect(await sessionManager.getSessionItem("someKey2")).toEqual(
+      "someValue2"
+    );
+    expect(await sessionManager.getSessionItem("access_token")).toEqual("asds");
 
     await sessionManager.destroySession();
 
-    expect(session.get("someKey")).toEqual("someValue");
-    expect(session.get("someKey2")).toEqual("someValue2");
-    expect(session.get("access_token")).not.toBeDefined();
+    expect(await sessionManager.getSessionItem("someKey")).toEqual("someValue");
+    expect(await sessionManager.getSessionItem("someKey2")).toEqual(
+      "someValue2"
+    );
+    expect(
+      await sessionManager.getSessionItem("access_token")
+    ).not.toBeDefined();
   });
 });
---
+++ tests/utils.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, test } from "vitest";
+import { createSessionManager } from "../src/session";
+import { generateCookieHeader } from "../src/utils/cookies";
+
+describe("Utils tests", () => {
+  test("serializeCookie returns headers object", async () => {
+    const mockRequest = new Request("http://kinde.com");
+    const { cookies, sessionManager } = await createSessionManager(mockRequest);
+    sessionManager.setSessionItem("someKey", "someValue");
+    const res = generateCookieHeader(mockRequest, cookies);
+    expect(res).toBeTypeOf("object");
+    const setCookie = res.getSetCookie();
+    expect(setCookie).toEqual([
+      "someKey=someValue; Path=/; HttpOnly; SameSite=Lax",
+    ]);
+  });
+
+  test("serializeCookie only adds to Set-Cookie when needed", async () => {
+    const mockRequest = new Request("http://kinde.com");
+    const { cookies, sessionManager } = await createSessionManager(mockRequest);
+    sessionManager.setSessionItem("someKey", "someValue");
+    sessionManager.removeSessionItem("someKey");
+    const res = generateCookieHeader(mockRequest, cookies);
+    expect(res).toBeTypeOf("object");
+    const setCookie = res.getSetCookie();
+    expect(setCookie).toStrictEqual([]);
+  });
+
+  test("serializeCookie handles deletion", async () => {
+    const mockRequest = new Request("http://kinde.com");
+    mockRequest.headers.append("Cookie", "someKey=someValue");
+    const { cookies, sessionManager } = await createSessionManager(mockRequest);
+    sessionManager.removeSessionItem("someKey");
+    const res = generateCookieHeader(mockRequest, cookies);
+    expect(res).toBeTypeOf("object");
+    const setCookie = res.getSetCookie();
+    expect(setCookie).toContain(
+      "someKey=0; Max-Age=-1; Path=/; HttpOnly; SameSite=Lax"
+    );
+  });
+});
